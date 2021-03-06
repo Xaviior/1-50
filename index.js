34c6ad799d69b4089e1717d990455a1dacbc12db
Xaviior <stiand.mail@gmail.com>
2021-03-07 00:03:48 +0100

    nada

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ const pickNumber = document.querySelector("#numberInput");
 const guessNumber = document.querySelector("#guessInput");
 // Number
 const allNumbers = document.querySelector(".numberswrapper");
-// The Game
-let isGameOver = false;
