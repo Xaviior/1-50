296b91819246a23a7db5732c8e8da4354e8b36cd
Xaviior <stiand.mail@gmail.com>
2021-03-07 11:26:06 +0100

    more const js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // inputs
 const pickNumber = document.querySelector("#numberInput");
 const guessNumber = document.querySelector("#guessInput");
+const picNumberBtn = document.querySelector("#btnSave");
+const guessNumberBtn = document.querySelector("#btnGuess");
 // Number
 const allNumbers = document.querySelectorAll(".number");
 let chosenNumber = 0;
