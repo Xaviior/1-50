6cfccd82f505fb41ae7beb8cc13e591220e071de
Xaviior <stiand.mail@gmail.com>
2021-03-07 11:22:52 +0100

    button id let js

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const pickNumber = document.querySelector("#numberInput");
 const guessNumber = document.querySelector("#guessInput");
 // Number
 const allNumbers = document.querySelectorAll(".number");
+let chosenNumber = 0;
